8601a7ac1f69f04a6d812302635d04e803f4da4b
km <35552602+catnap7777@users.noreply.github.com>
2020-09-02 22:15:45 -0500

    initial commit

--- MAD157Module3BasicInteraction2KarenM/ViewController.swift
+++ MAD157Module3BasicInteraction2KarenM/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var labelResult: UILabel!
+    @IBOutlet var leftButton: UIButton!
+    @IBOutlet var rightButton: UIButton!
+    
+    let leftMsg = "Left button clicked..."
+    let rightMsg = "Right button clicked..."
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func leftButtonClick(_ sender: UIButton) {
+        labelResult.backgroundColor = UIColor.magenta
+        labelResult.text = leftMsg.uppercased()
+        labelResult.font = UIFont.boldSystemFont(ofSize: 17.0)
+        leftButton.backgroundColor = UIColor.magenta
+        leftButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        leftButton.setTitle("LEFT", for: UIControl.State.normal)
+        rightButton.setTitle("right", for: UIControl.State.normal)
+        rightButton.backgroundColor = UIColor.lightGray
+        
+    }
+        
 
+    @IBAction func rightButtonClick(_ sender: UIButton) {
+        labelResult.backgroundColor = UIColor.green
+        labelResult.text = rightMsg.uppercased()
+        labelResult.font = UIFont.boldSystemFont(ofSize: 17.0)
+        rightButton.backgroundColor = UIColor.green
+        rightButton.setTitleColor(UIColor.white, for: UIControl.State.normal)
+        rightButton.setTitle("RIGHT", for: UIControl.State.normal)
+        leftButton.setTitle("left", for: UIControl.State.normal)
+        leftButton.backgroundColor = UIColor.lightGray
+        
+    }
+    
 
 }
 
